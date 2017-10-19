66434edcf52c15601e6c32bc4a909e0ac48810c0
feat7 <vinaykhobragade99@gmail.com>
2017-10-20 02:12:35 +0530

    server created

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 /**
- * ES6 Added
+ * app.js
  */
 import express from 'express'
+import easyrtc from 'easyrtc'
+import ejs from 'ejs'
+import io from 'socket.io'
+import nconf from 'nconf'
+import fs from 'fs'
 
-let app = express()
+nconf.file('app.config.json');
+
+let app = express();
+
+
+var port = nconf.get('port');
+
+let server = require('http').createServer(app).listen(port);
